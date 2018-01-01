2fe3d3ff7bee522a4ef87af7072e29200e7626d3
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-01-01 16:59:46 +0100

    Update Checkstyle to 8.6

--- src/main/groovy/com/vanniktech/code/quality/tools/CodeQualityToolsPluginExtension.groovy
+++ src/main/groovy/com/vanniktech/code/quality/tools/CodeQualityToolsPluginExtension.groovy
@@ -72,7 +72,7 @@ class CodeQualityToolsPluginExtension {
     boolean enabled = true
 
     /** @since 0.2.0 */
-    String toolVersion = '7.8.2'
+    String toolVersion = '8.6'
 
     /** @since 0.2.0 */
     String configFile = 'code_quality_tools/checkstyle.xml'
